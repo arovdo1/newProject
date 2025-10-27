ebd3eacc3b3c1ae0ae608634a7b86b809dad325f
Rovdo Anna <arovdo1@gmail.com>
2025-10-27 14:15:03 +0300

    добавлен итеративный метод

--- Main.java
+++ Main.java
@@ -9,4 +9,12 @@ public class Main {
         if (n == 0 || n == 1) return 1;
         return n * factorialRecursive(n - 1);
     }
+
+    public static long factorialIterative(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
