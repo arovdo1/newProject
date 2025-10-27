06eacbb180d784ad8cc72d703489919b16842167
Rovdo Anna <arovdo1@gmail.com>
2025-10-27 14:10:21 +0300

    Добавлен вызов рекурсивной функции

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир! Hello Vitaly!");
+
+        System.out.println("5! (рекурсия) = " + factorialRecursive(5));
     }
     public static long factorialRecursive(int n) {
         if (n == 0 || n == 1) return 1;
