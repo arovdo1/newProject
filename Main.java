4f0798cdde8094c35dd24aa50303477111679593
Rovdo Anna <arovdo1@gmail.com>
2025-10-27 14:07:24 +0300

    Добавлен рекурсивный метод

--- Main.java
+++ Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир! Hello Vitaly!");
     }
+    public static long factorialRecursive(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factorialRecursive(n - 1);
+    }
 }
