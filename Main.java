a33dcf40c9ebdd35e162f649214e6e810e4c2d55
Rovdo Anna <arovdo1@gmail.com>
2025-10-27 14:20:08 +0300

    добавлен вызов итеративного метода

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Привет, мир! Hello Vitaly!");
 
         System.out.println("5! (рекурсия) = " + factorialRecursive(5));
+        System.out.println("5! (итерация) = " + factorialIterative(5));
     }
     public static long factorialRecursive(int n) {
         if (n == 0 || n == 1) return 1;
